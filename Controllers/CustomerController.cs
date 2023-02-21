2a7b10c6e960a304858e238f8008c1deb4b4dd86
luan200292 <luanlggcc200292@fpt.edu.vn>
2023-02-21 11:12:47 +0700

    Add function Change Information User in Profile

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -48,5 +48,56 @@ namespace FPTBook.Controllers
             }
             return View(result);
         }
+        
+        public async Task<IActionResult> Update(string id)
+        {
+            if(id == null || userManager.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(User model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                user.full_name = model.full_name;
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                user.gender = model.gender;
+                user.PhoneNumber = model.PhoneNumber;
+                user.PhoneNumberConfirmed = true;
+                user.address = model.address;
+
+            }
+            var result = await userManager.UpdateAsync(user);
+
+            if(result.Succeeded)
+            {
+                TempData["msg"] = "Updated successfully!";
+                return RedirectToAction(nameof(Update));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("",error.Description);
+            }
+
+            return View(model);
+        }
     }
 }
