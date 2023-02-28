0a1a734f425cb887824885bc1ad24bc1868f8338
nhanks1412 <106572190+nhanks1412@users.noreply.github.com>
2023-02-28 16:40:25 +0700

    Add function View Books in homepage

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,23 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FPTBook.Models;
+using FPTBook.DB;
 
 namespace FPTBook.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ApplicationDbContext db)
     {
-        _logger = logger;
+        this._db = db;
     }
-
     public IActionResult Index()
     {
-        return View();
+        var ds = _db.Books;
+        var lst = ds.Where(b => b.status == 1).ToList();
+        return View(lst);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
