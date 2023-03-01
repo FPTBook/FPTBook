518c8d40930105ad4af1425647e0f045045209a2
luan200292 <luanlggcc200292@fpt.edu.vn>
2023-03-01 23:57:29 +0700

    Add function approve and reject request add category

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -24,15 +24,41 @@ namespace FPTBook.Controllers
         {
             return View();
         }
+
         public IActionResult ViewListAccounts()
         {
             var lstAcc = _db.Users.ToList();
             return View(lstAcc);
         }
+
         public IActionResult CategoryApproval()
         {
             var lstApp = _db.Category_Requests.Include(c => c.user).ToList();
             return View(lstApp);
+        }
+
+        public IActionResult ApproveRequest(int id)
+        {
+            var obj = _db.Category_Requests.Find(id);
+            obj.status = 1;
+            var cate = _db.Categories.Where(c => c.name == obj.description).FirstOrDefault();
+            cate.status = 1;
+            _db.Update(cate);
+            _db.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(CategoryApproval));
+        }
+        
+        public IActionResult RejectRequest(int id)
+        {
+            var obj = _db.Category_Requests.Find(id);
+            obj.status = 1;
+            var cate = _db.Categories.Where(c => c.name == obj.description).FirstOrDefault();
+            cate.status = 2;
+            _db.Update(cate);
+            _db.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(CategoryApproval));
         }
     }
     
