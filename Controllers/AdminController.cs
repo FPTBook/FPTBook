eda17ffcc9c8b9e1b25648a21886f0d30bdd9119
nhanks1412 <106572190+nhanks1412@users.noreply.github.com>
2023-03-01 22:25:51 +0700

    Add function view category approval

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using FPTBook.DB;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,11 @@ namespace FPTBook.Controllers
     [Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public AdminController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
         public IActionResult Index()
         {
             return View();
