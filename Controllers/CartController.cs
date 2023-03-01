3736be8543e3ba5a816a2f36fb907d4e777ddb19
kienltgcc200184 <kienltgcc200184@fpt.edu.vn>
2023-03-01 17:06:18 +0700

    Add function add book to cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -31,6 +31,29 @@ namespace FPTBook.Controllers
             return View(cart);
         }
 
+        public async Task<IActionResult> AddCart(int book_id)
+        {
+            var book = await _db.Books.Where(b => b.id == book_id).FirstOrDefaultAsync();
+            string userId = GetUserId();
+            var user = await _db.Users.Where(u => u.Id == userId).FirstOrDefaultAsync();
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _db.Add(new Cart()
+            {
+                book_id = book_id,
+                book = book,
+                user = user,
+                user_id = userId,
+                quantity = 1,
+                date = DateTime.Now
+            });
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         public async Task<IActionResult> DeleteCart(int? id)
         {
             var cartItem = await _db.Carts.FindAsync(id);
@@ -41,7 +64,7 @@ namespace FPTBook.Controllers
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        
+
         private string GetUserId()
         {
             return Convert.ToString(HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
